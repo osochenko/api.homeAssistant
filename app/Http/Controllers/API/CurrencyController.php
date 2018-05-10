f33c65cf49b7ab730dd9ff440f87403a6aeb3e2a
osochenko <vadim.osochenko@gmail.com>
2018-05-10 20:39:02 +0300

    ADD: resource for currency entity

--- app/Http/Controllers/API/CurrencyController.php
+++ app/Http/Controllers/API/CurrencyController.php
@@ -5,19 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\API;
 
 use App\Models\Currency;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use App\Transformers\CurrencyTransformer;
+use App\Http\Resources\Currency as CurrencyResource;
 
 class CurrencyController extends Controller
 {
     /**
      * Display a listing of the Currency.
-     *
-     * @return JsonResponse
      */
-    public function index(): JsonResponse
+    public function index()
     {
-        return response()->json(fractal(Currency::all(), new CurrencyTransformer()));
+        return CurrencyResource::collection(Currency::all());
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\Resource;
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Resource::withoutWrapping();
     }
 
     /**
